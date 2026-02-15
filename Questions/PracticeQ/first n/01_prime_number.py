ca067788cb1303b208909008d07f6c0f3dfbeb22
prasad1057 <khandagaleprasad188@gmail.com>
2026-02-16 01:21:53 +0530

    done questions

--- Questions/PracticeQ/first n/01_prime_number.py
+++ Questions/PracticeQ/first n/01_prime_number.py
@@ -4,17 +4,18 @@ Q. Write a program to print the first n prime numbers.
 '''
 
 num = int(input('Enter the number: '))
-n = 2
-count = 0
 
-while(count < n):
+count = 0
+n = 2
+
+while(count < num):         # loop till last condition (5 < 5)
     
-    for i in range(2,n):
-        if num % i == 0:
-            break
+    for i in range(2,n):        # 
+        if n % i == 0:      # if loop is break in between then else bloack will not execute
+            break       # if break statement is execute firstly and lastly then n only then else bloack will execute 
     else:
-        print(num,end=' ')
-        count += 1
+        print(n,end=',')
+        count += 1          # c is increases to check further condition (5 < 5) 
         
-    num += 1
+    n += 1
     
--- Questions/PracticeQ/first n/02_pallindrome.py
+++ Questions/PracticeQ/first n/02_pallindrome.py
@@ -1,4 +1,19 @@
 '''
 Q. Write a program to find and print the first n palindrome numbers (starting from 0 or a specific range).
-(Example: If $n=12$, output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22)
+(Example: If n=12, output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22)
 '''
+
+num = int(input('Enter the number: '))
+
+count = 0
+
+while count < num:
+    
+    for i in range(1,num):
+        rem = num % 10          # last digit
+        num = num // 10         # reamining digits
+
+        count += 1
+    print(count)
+    
+    
