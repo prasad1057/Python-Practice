229fe329ec9284e5cb513921581f7df076b4df2d
prasad1057 <khandagaleprasad188@gmail.com>
2026-02-19 00:53:38 +0530

    done questions

--- Questions/PracticeQ/first n/01_prime_number.py
+++ Questions/PracticeQ/first n/01_prime_number.py
@@ -12,7 +12,7 @@ while(count < num):         # loop till last condition (5 < 5)
     
     for i in range(2,n):        # 
         if n % i == 0:      # if loop is break in between then else bloack will not execute
-            break       # if break statement is execute firstly and lastly then n only then else bloack will execute 
+            break           # if break statement is execute firstly and lastly then n only then else bloack will execute 
     else:
         print(n,end=',')
         count += 1          # c is increases to check further condition (5 < 5) 
--- Questions/PracticeQ/first n/02_pallindrome.py
+++ Questions/PracticeQ/first n/02_pallindrome.py
@@ -3,17 +3,24 @@ Q. Write a program to find and print the first n palindrome numbers (starting fr
 (Example: If n=12, output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22)
 '''
 
-num = int(input('Enter the number: '))
+
+n = int(input("Enter how many palindrome numbers you want: "))
 
 count = 0
+num = 0
 
-while count < num:
-    
-    for i in range(1,num):
-        rem = num % 10          # last digit
-        num = num // 10         # reamining digits
+while count < n:
+    original = num
+    reverse = 0
+    temp = num
 
+    while temp > 0:
+        digit = temp % 10
+        reverse = reverse * 10 + digit
+        temp = temp // 10
+
+    if original == reverse:
+        print(original, end=" ")
         count += 1
-    print(count)
-    
-    
+
+    num += 1
